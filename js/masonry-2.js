bb229b58c63ed8b69e1dddf4ed153e8326835de8
Andy Garcia <andygarcia@live.fr>
2023-07-21 15:47:06 +0200

    [feature]-- adding a new masonry

---
+++ js/masonry-2.js
@@ -0,0 +1,14 @@
+
+var grid = document.querySelector('.grid');
+var msnry = new Masonry(grid, {
+  itemSelector: 'img',
+  columnWidth: '.grid-sizer',
+  gutter: 10
+});
+
+var grid = document.querySelector('.grid');
+var msnry = new Masonry(grid, {
+  itemSelector: 'img',
+  columnWidth: '.grid-sizer',
+  gutter: 10
+});
